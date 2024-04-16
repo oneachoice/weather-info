f9c5567433ef559b9d618dfe3acad9b0308c1a46
Wonhyuk Choi <oneachoice@gmail.com>
2024-04-16 13:22:35 +0900

    Feat: CurretWeather 컴포넌트 생성
    CurrentWeather 컴포넌트의 기본 틀 제작.
    
    아래와 같은 항목이 생성됨.
    - 날짜와 시간 항목
    - 날씨 기호와 온도 항목

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import { fontDoHyeon } from "@/fonts/dohyeon";
+import styles from "./Home.module.scss";
+
 import Head from "next/head";
 
 export default function Home() {
@@ -5,11 +8,39 @@ export default function Home() {
     <>
       <Head>
         <title>WEATHER INFO</title>
-        <meta name="description" content="This web app informs you of weather information." />
+        <meta
+          name="description"
+          content="This web app informs you of weather information."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className={styles["page-container"]}>
+        <CurrentWeather />
+      </main>
     </>
+  );
+}
+
+function CurrentWeather() {
+  return (
+    <div className={styles["current-weather"]} style={fontDoHyeon.style}>
+      <div className={styles["datetime"]}>
+        <p className={styles["datetime__date"]}>2024-04-16</p>
+        <p className={styles["datetime__time"]}>
+          <span>오후</span> 12:06
+        </p>
+      </div>
+      <div className={styles["weather-symbol"]}>
+        <img
+          className={styles["weather-icon"]}
+          src="https://openweathermap.org/img/wn/11d@2x.png"
+        />
+        <div className={styles["temperature"]}>
+          21
+          <span className={styles["temperature__unit"]}>℃</span>
+        </div>
+      </div>
+    </div>
   );
 }
