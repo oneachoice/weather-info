c7405083c1f236cf5b43b2069fc2ca86284a9eb8
Wonhyuk Choi <oneachoice@gmail.com>
2024-04-18 21:55:50 +0900

    Feat: CurrentWeather 컴포넌트, 날씨 정보 추가
    추가된 기능
    - 위치
    - 날씨 설명
    - 체감온도
    - 습도
    - 풍향
    - 풍속

--- src/components/weather/CurrentWeather.tsx
+++ src/components/weather/CurrentWeather.tsx
@@ -3,11 +3,23 @@ import styles from "./CurrentWeather.module.scss";
 
 import { fontDoHyeon } from "@/fonts/dohyeon";
 import { getFullDate, getFullTime } from "@/util/date";
+import useCurrentWeatherData from "@/hooks/useCurrentWeatherData";
+import Image from "next/image";
 
 export default function CurrentWeather() {
   const currentDate = useCurrentDate();
   const fullDate = getFullDate(currentDate);
   const fullTime = getFullTime(currentDate);
+
+  const [currentWeatherData, refresh] = useCurrentWeatherData();
+  
+  let imgSrc = require("@/assets/openweathermap/not-available.svg");
+  let temperature: number | string = "-";
+
+  if (currentWeatherData) {
+    imgSrc = require(`@/assets/openweathermap/${currentWeatherData.weather[0].icon}.svg`);
+    temperature = Math.round(currentWeatherData.main.temp);
+  }
 
   return (
     <div className={styles["current-weather"]} style={fontDoHyeon.style}>
@@ -16,14 +28,39 @@ export default function CurrentWeather() {
         <p className={styles["datetime__time"]}>{fullTime}</p>
       </div>
       <div className={styles["weather-symbol"]}>
-        <img
-          className={styles["weather-icon"]}
-          src="https://openweathermap.org/img/wn/11d@2x.png"
-        />
+        <Image className={styles["weather-icon"]} src={imgSrc} alt="weather icon" />
         <div className={styles["temperature"]}>
-          21
+          {temperature}
           <span className={styles["temperature__unit"]}>℃</span>
         </div>
+      </div>
+      <div className={styles["weather-info"]}>
+        <table>
+          <tr>
+            <td>위치</td>
+            <td>{currentWeatherData?.name}</td>
+          </tr>
+          <tr>
+            <td>날씨</td>
+            <td>{currentWeatherData?.weather[0].description}</td>
+          </tr>
+          <tr>
+            <td>체감온도</td>
+            <td>{currentWeatherData?.main.feels_like}℃</td>
+          </tr>
+          <tr>
+            <td>습도</td>
+            <td>{currentWeatherData?.main.humidity}%</td>
+          </tr>
+          <tr>
+            <td>풍향</td>
+            <td>{currentWeatherData?.wind.deg}°</td>
+          </tr>
+          <tr>
+            <td>풍속</td>
+            <td>{currentWeatherData?.wind.speed}m/s</td>
+          </tr>
+        </table>
       </div>
     </div>
   );
