2e22295b82af7884e5810e7c80cb4b8be301079b
Wonhyuk Choi <oneachoice@gmail.com>
2024-04-18 21:48:03 +0900

    Feat: getFullDate 반환 값 포맷 수정
    'N요일, YYYY년 M월 D일' 형식으로 수정함.

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,11 +1,9 @@
 import { changeDecimalFormat } from "./format";
 
 export function getFullDate(date: Date) {
-  const year = date.getFullYear();
-  const month = changeDecimalFormat(date.getMonth() + 1, 2);
-  const day = changeDecimalFormat(date.getDay(), 2);
-
-  return `${year}-${month}-${day}`;
+  return `${getWeekDay(date)}요일, ${date.getFullYear()}년 ${
+    date.getMonth() + 1
+  }월 ${date.getDay()}일`;
 }
 
 export function getFullTime(date: Date) {
@@ -17,4 +15,25 @@ export function getFullTime(date: Date) {
   const minutes = changeDecimalFormat(date.getMinutes(), 2);
 
   return `${meridiem} ${hours}:${minutes}`;
+}
+
+export function getWeekDay(date: Date) {
+  switch (date.getDay()) {
+    case 0:
+      return "일";
+    case 1:
+      return "월";
+    case 2:
+      return "화";
+    case 3:
+      return "수";
+    case 4:
+      return "목";
+    case 5:
+      return "금";
+    case 6:
+      return "토";
+    default:
+      return "?";
+  }
 }
