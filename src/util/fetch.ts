49fcb2a99298984777cd2e08c15de523cb68ddc9
Wonhyuk Choi <oneachoice@gmail.com>
2024-04-20 19:01:14 +0900

    Feat: fetchCurrentWeatherData 내부 로직 변경
    기존 fetch로 불러오던 데이터를 axios를 사용해서 가져옴.

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,7 +1,13 @@
+import axios from "axios";
+
 const DOMAIN = "http://localhost:3000";
 
 export async function fetchCurrentWeatherData(coords: GeolocationCoordinates) {
-  return await fetch(
-    `${DOMAIN}/api/current-weather?latitude=${coords.latitude}&longitude=${coords.longitude}`
-  );
+  const response =  await axios.get(`/api/current-weather`, {
+    params: {
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    }
+  } )
+  return response.data;
 }
