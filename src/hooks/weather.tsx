b524ab16af18e20b7425291693c0ad97fc0aa128
Wonhyuk Choi <oneachoice@gmail.com>
2024-04-23 19:07:05 +0900

    Feat: useForecastData 훅 추가
    위 훅은 Forecast 데이터를 api로부터 받아 반환함.

--- src/hooks/weather.tsx
+++ src/hooks/weather.tsx
@@ -1,18 +1,32 @@
-import { CurrentWeatherData } from "@/interfaces/openweathermap-api";
-import { fetchCurrentWeatherData } from "@/util/fetch";
+import {
+  CurrentWeatherData,
+  ForecastData,
+} from "@/interfaces/openweathermap-api";
+import { fetchCurrentWeatherData, fetchForecastData } from "@/util/fetch";
 import { getCoords } from "@/util/location";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 export function useCurrentWeatherData() {
   const [currentWeatherData, setCurrentWeatherData] =
     useState<CurrentWeatherData>();
 
-  // Initilize the currentWeatherData
-  useEffect(() => {
-    (async () => {
-      setCurrentWeatherData(await fetchCurrentWeatherData(await getCoords()));
-    })();
-  }, []);
+  useInit(setCurrentWeatherData);
 
   return currentWeatherData;
+}
+
+export function useForecastData() {
+  const [forecastData, setForecastData] = useState<ForecastData>();
+
+  useInit(setForecastData);
+
+  return forecastData;
+}
+
+function useInit(dispatch: Dispatch<SetStateAction<any>>) {
+  useEffect(() => {
+    (async () => {
+      dispatch(await fetchCurrentWeatherData(await getCoords()));
+    })();
+  }, [dispatch]);
 }
